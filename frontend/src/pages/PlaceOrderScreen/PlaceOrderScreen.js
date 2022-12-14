01eae025b583a22ee4b419547fd0a3e938999fbc
Luca Novello <lucanovelloto@gmail.com>
2022-12-14 00:54:54 -0500

    actual final clean up, unused variables

--- frontend/src/pages/PlaceOrderScreen/PlaceOrderScreen.js
+++ frontend/src/pages/PlaceOrderScreen/PlaceOrderScreen.js
@@ -1,6 +1,6 @@
 import { Fragment, useContext, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import CheckoutSteps from '../../components/CheckoutSteps/CheckoutSteps';
 import { Store } from '../../Store';
 import PlaceOrderAddress from './PlaceOrderAddress';
@@ -10,69 +10,59 @@ import placeOrderScreenStyle from './PlaceOrderScreen.module.css';
 import PlaceOrderSummary from './PlaceOrderSummary';
 
 const PlaceOrderScreen = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { cart, userInfo } = state;
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const { cart, userInfo } = state;
 
-  cart.itemsPrice = cart.cartItems
-    .reduce((a, c) => a + c.price * c.quantity, 0)
-    .toFixed(2);
-  cart.shippingPrice = (0).toFixed(2);
-  cart.taxPrice = (
-    cart.cartItems.reduce((a, c) => a + c.price * c.quantity, 0) * 0.13
-  ).toFixed(2);
-  cart.totalPrice = (
-    parseFloat(cart.itemsPrice) +
-    parseFloat(cart.shippingPrice) +
-    parseFloat(cart.taxPrice)
-  ).toFixed(2);
+    cart.itemsPrice = cart.cartItems.reduce((a, c) => a + c.price * c.quantity, 0).toFixed(2);
+    cart.shippingPrice = (0).toFixed(2);
+    cart.taxPrice = (cart.cartItems.reduce((a, c) => a + c.price * c.quantity, 0) * 0.13).toFixed(
+        2
+    );
+    cart.totalPrice = (
+        parseFloat(cart.itemsPrice) +
+        parseFloat(cart.shippingPrice) +
+        parseFloat(cart.taxPrice)
+    ).toFixed(2);
 
-  useEffect(() => {
-    if (!userInfo) {
-      return navigate('/signin');
-    }
-    if (cart.cartItems.length < 1) {
-      return navigate('/');
-    }
-    if (cart.shippingAddress.length < 1) {
-      return navigate('/shipping');
-    }
-    if (cart.paymentMethod.length < 1) {
-      return navigate('/payment');
-    }
-  }, [cart, navigate, userInfo]);
+    useEffect(() => {
+        if (!userInfo) {
+            return navigate('/signin');
+        }
+        if (cart.cartItems.length < 1) {
+            return navigate('/');
+        }
+        if (cart.shippingAddress.length < 1) {
+            return navigate('/shipping');
+        }
+        if (cart.paymentMethod.length < 1) {
+            return navigate('/payment');
+        }
+    }, [cart, navigate, userInfo]);
 
-  return (
-    <Fragment>
-      <Helmet>
-        <title>Preview Order</title>
-      </Helmet>
-      <CheckoutSteps step1 step2 step3 step4 />
+    return (
+        <Fragment>
+            <Helmet>
+                <title>Preview Order</title>
+            </Helmet>
+            <CheckoutSteps step1 step2 step3 step4 />
 
-      <div className={placeOrderScreenStyle.placeOrderScreenContainer}>
-        <h2 className={placeOrderScreenStyle.placeOrderScreenTitle}>
-          Preview Order
-        </h2>
+            <div className={placeOrderScreenStyle.placeOrderScreenContainer}>
+                <h2 className={placeOrderScreenStyle.placeOrderScreenTitle}>Preview Order</h2>
 
-        <div className={placeOrderScreenStyle.placeOrderScreenInnerContainer}>
-          <div
-            className={placeOrderScreenStyle.placeOrderScreenInnerContainerLeft}
-          >
-            <PlaceOrderAddress cart={cart} />
-            <PlaceOrderPayment cart={cart} />
-            <PlaceOrderItems cart={cart.cartItems} ctxDispatch={ctxDispatch} />
-          </div>
+                <div className={placeOrderScreenStyle.placeOrderScreenInnerContainer}>
+                    <div className={placeOrderScreenStyle.placeOrderScreenInnerContainerLeft}>
+                        <PlaceOrderAddress cart={cart} />
+                        <PlaceOrderPayment cart={cart} />
+                        <PlaceOrderItems cart={cart.cartItems} ctxDispatch={ctxDispatch} />
+                    </div>
 
-          <PlaceOrderSummary
-            cart={cart}
-            userInfo={userInfo}
-            ctxDispatch={ctxDispatch}
-          />
-        </div>
-      </div>
-    </Fragment>
-  );
+                    <PlaceOrderSummary cart={cart} userInfo={userInfo} ctxDispatch={ctxDispatch} />
+                </div>
+            </div>
+        </Fragment>
+    );
 };
 
 export default PlaceOrderScreen;
--- frontend/src/pages/ProfileScreen/ProfileScreen.js
+++ frontend/src/pages/ProfileScreen/ProfileScreen.js
@@ -7,143 +7,134 @@ import { getError } from '../../utils';
 import profileScreenStyle from './ProfileScreen.module.css';
 
 const reducer = (state, action) => {
-  switch (action.type) {
-    case 'UPDATE_REQUEST':
-      return { ...state, loadingUpdate: true };
-    case 'UPDATE_SUCCESS':
-      return { ...state, loadingUpdate: false };
-    case 'UPDATE_FAIL':
-      return { ...state, loadingUpdate: false };
+    switch (action.type) {
+        case 'UPDATE_REQUEST':
+            return { ...state, loadingUpdate: true };
+        case 'UPDATE_SUCCESS':
+            return { ...state, loadingUpdate: false };
+        case 'UPDATE_FAIL':
+            return { ...state, loadingUpdate: false };
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 };
 
 function ProfileScreen() {
-  const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { userInfo } = state;
-  const [name, setName] = useState(userInfo.name);
-  const [email, setEmail] = useState(userInfo.email);
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const { userInfo } = state;
+    const [name, setName] = useState(userInfo.name);
+    const [email, setEmail] = useState(userInfo.email);
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
-  const [{ loadingUpdate }, dispatch] = useReducer(reducer, {
-    loadingUpdate: false,
-  });
+    const [{ loadingUpdate }, dispatch] = useReducer(reducer, {
+        loadingUpdate: false,
+    });
 
-  const onSubmitHandler = async (e) => {
-    e.preventDefault();
-    if (password !== confirmPassword) {
-      toast.error('Passwords do not match');
-      return;
-    }
-    try {
-      const { data } = await axios.put(
-        '/api/users/profile',
-        {
-          name,
-          email,
-          password,
-        },
-        {
-          headers: { Authorization: `Bearer ${userInfo.token}` },
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            toast.error('Passwords do not match');
+            return;
         }
-      );
-      dispatch({
-        type: 'UPDATE_SUCCESS',
-      });
-      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
-      localStorage.setItem('userInfo', JSON.stringify(data));
-      toast.success('Profile Updated');
-    } catch (err) {
-      dispatch({
-        type: 'FETCH_FAIL',
-      });
-      toast.error(getError(err));
-    }
-  };
+        try {
+            const { data } = await axios.put(
+                '/api/users/profile',
+                {
+                    name,
+                    email,
+                    password,
+                },
+                {
+                    headers: { Authorization: `Bearer ${userInfo.token}` },
+                }
+            );
+            dispatch({
+                type: 'UPDATE_SUCCESS',
+            });
+            ctxDispatch({ type: 'USER_SIGNIN', payload: data });
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            toast.success('Profile Updated');
+        } catch (err) {
+            dispatch({
+                type: 'FETCH_FAIL',
+            });
+            toast.error(getError(err));
+        }
+    };
 
-  return (
-    <div className={profileScreenStyle.profileScreenContainer}>
-      <Helmet>
-        <title>User Profile</title>
-      </Helmet>
-      <h2 className={profileScreenStyle.profileTitle}>{`User Profile`}</h2>
-      <form
-        className={profileScreenStyle.profileContainer}
-        onSubmit={onSubmitHandler}
-      >
-        <div className={profileScreenStyle.profileWrapper}>
-          <label htmlFor={'name'} className={profileScreenStyle.profileLabel}>
-            Name
-          </label>
-          <input
-            type={'text'}
-            id={'name'}
-            value={name}
-            className={profileScreenStyle.profileInput}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
+    return (
+        <div className={profileScreenStyle.profileScreenContainer}>
+            <Helmet>
+                <title>User Profile</title>
+            </Helmet>
+            <h2 className={profileScreenStyle.profileTitle}>{`User Profile`}</h2>
+            <form className={profileScreenStyle.profileContainer} onSubmit={onSubmitHandler}>
+                <div className={profileScreenStyle.profileWrapper}>
+                    <label htmlFor={'name'} className={profileScreenStyle.profileLabel}>
+                        Name
+                    </label>
+                    <input
+                        type={'text'}
+                        id={'name'}
+                        value={name}
+                        className={profileScreenStyle.profileInput}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div className={profileScreenStyle.profileWrapper}>
+                    <label htmlFor={'email'} className={profileScreenStyle.profileLabel}>
+                        Email address
+                    </label>
+                    <input
+                        type={'email'}
+                        id={'email'}
+                        value={email}
+                        className={profileScreenStyle.profileInput}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div className={profileScreenStyle.profileWrapper}>
+                    <label htmlFor={'password'} className={profileScreenStyle.profileLabel}>
+                        New Password
+                    </label>
+                    <input
+                        type={'password'}
+                        id={'password'}
+                        className={profileScreenStyle.profileInput}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        autoComplete={'off'}
+                        required
+                    />
+                </div>
+
+                <div className={profileScreenStyle.profileWrapper}>
+                    <label htmlFor={'confirmPassword'} className={profileScreenStyle.profileLabel}>
+                        Confirm New Password
+                    </label>
+                    <input
+                        type={'password'}
+                        id={'confirmPassword'}
+                        className={profileScreenStyle.profileInput}
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        autoComplete={'off'}
+                        required
+                    />
+                </div>
+
+                <button type="submit" className={profileScreenStyle.profileButton}>
+                    Update Profile
+                </button>
+            </form>
         </div>
-
-        <div className={profileScreenStyle.profileWrapper}>
-          <label htmlFor={'email'} className={profileScreenStyle.profileLabel}>
-            Email address
-          </label>
-          <input
-            type={'email'}
-            id={'email'}
-            value={email}
-            className={profileScreenStyle.profileInput}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-
-        <div className={profileScreenStyle.profileWrapper}>
-          <label
-            htmlFor={'password'}
-            className={profileScreenStyle.profileLabel}
-          >
-            New Password
-          </label>
-          <input
-            type={'password'}
-            id={'password'}
-            className={profileScreenStyle.profileInput}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            autoComplete={'off'}
-            required
-          />
-        </div>
-
-        <div className={profileScreenStyle.profileWrapper}>
-          <label
-            htmlFor={'confirmPassword'}
-            className={profileScreenStyle.profileLabel}
-          >
-            Confirm New Password
-          </label>
-          <input
-            type={'password'}
-            id={'confirmPassword'}
-            className={profileScreenStyle.profileInput}
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            autoComplete={'off'}
-            required
-          />
-        </div>
-
-        <button type="submit" className={profileScreenStyle.profileButton}>
-          Update Profile
-        </button>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default ProfileScreen;
